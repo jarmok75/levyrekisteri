8bb7f0365d15964e2786516ff627b056ca489068
jarmok75 <74949389+jarmok75@users.noreply.github.com>
2022-04-11 13:17:06 +0300

    Add files via upload
    Levy luokan päivitys

--- Levy.py
+++ Levy.py
@@ -4,11 +4,11 @@ from PySide2.QtWidgets import QApplication, QMainWindow
 from levyrekisteri import Ui_LevyRekisteri
 #Uuden ikkunan takia
 from tallenna import Ui_Tallenna
-from tallenna_gui import Tallenna_gui
+#from tallenna_gui import Tallenna_gui
 
 #Luokka sisältää kaikki Levyn tiedot
 
-class Levy():
+class Levy(object):
     def __init__(self,Artisti="",LevynNimi="",JulkV="",Levy_Yht="",Painos=""):
         super().__init__()
         
@@ -18,4 +18,7 @@ class Levy():
         self.JulkaisuVuosi = JulkV
         self.Levy_yhtio = Levy_Yht
         self.Painos = Painos
+
+    
+        
         
